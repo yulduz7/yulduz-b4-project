720ac3222b82c275bed5da0f32b36c0ac1e3b3c9
yulduzakabirova <akabirovayulduz@gmail.com>
2021-08-29 09:54:35 -0400

    modified Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("This is Yulduz's project");
+        System.out.println("This is Yulduz's project");
+        System.out.println(revStr("Hello World"));
     }
+
+    public static String revStr(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
 }
